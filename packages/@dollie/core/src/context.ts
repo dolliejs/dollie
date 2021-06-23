528d808850f23e72c366e9dd87a1a3d2817bb651
lenconda <i@lenconda.top>
2021-06-23 15:41:16 +0800

    feat(context): optimize lifecycle

--- packages/@dollie/core/src/context.ts
+++ packages/@dollie/core/src/context.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import Generator from './generator';
 import { DollieConfig } from './interfaces';
 
@@ -11,6 +12,17 @@ class Context {
   ) {}
 
   public async generate() {
+    const lifecycleList = ['bootstrap', 'load', 'write', 'conflict', 'end'];
+    for (const lifecycle of lifecycleList) {
+      const lifecycleExecutor = this[lifecycle];
+      if (_.isFunction(lifecycleExecutor)) {
+        await lifecycleExecutor.call(this);
+      }
+    }
+    return this.generator.getResult();
+  }
+
+  protected bootstrap() {
     const {
       projectName,
       templateOriginName,
@@ -20,14 +32,25 @@ class Context {
     this.generator.checkInputs();
     this.generator.initialize();
     this.generator.checkContext();
+  }
+
+  protected async load() {
     await this.generator.loadTemplate();
     await this.generator.queryAllTemplateProps();
+  }
+
+  protected write() {
     this.generator.copyTemplateFileToCacheTable();
     this.generator.deleteFiles();
     this.generator.mergeTemplateFiles();
+  }
+
+  protected async conflict() {
     await this.generator.resolveConflicts();
+  }
+
+  protected async end() {
     await this.generator.runCleanups();
-    return this.generator.getResult();
   }
 }
 
