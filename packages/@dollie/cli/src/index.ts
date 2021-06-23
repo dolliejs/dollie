55cd767547d98322e7b0a69bf47efd5565575faf
lenconda <i@lenconda.top>
2021-06-23 20:18:00 +0800

    fix: generator config

--- packages/@dollie/cli/src/index.ts
+++ packages/@dollie/cli/src/index.ts
@@ -3,8 +3,10 @@ import inquirer from 'inquirer';
 
 async function test() {
   const context = new Context('test', 'lenconda/template-react', {
-    getTemplateProps: async (questions) => {
-      return await inquirer.prompt(questions);
+    generator: {
+      getTemplateProps: async (questions) => {
+        return await inquirer.prompt(questions);
+      },
     },
   });
   await context.generate();
