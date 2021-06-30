72dd9cf7ce145c373290fb6575fde49b1d92b4e4
lenconda <i@lenconda.top>
2021-06-30 14:16:34 +0800

    feat: add and optimize docs

--- .umirc.ts
+++ .umirc.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   base: 'docs',
   hash: true,
   locales: [
-    ['en', 'English'],
-    ['zh', '中文'],
+    ['en-US', 'English'],
+    ['zh-CN', '中文'],
   ],
   copy: [
     {
@@ -38,10 +38,17 @@ export default defineConfig({
     '.__dumi-default-menu-inner ul li a::before, .__dumi-default-menu-inner ul li > span::before { display: none !important; }',
     '.__dumi-default-layout-hero button { border-radius: 4px !important; }',
     '.__dumi-default-menu[data-mode=\'site\'] { background: white !important; }',
-    'code, pre { color: #844d28 !important; font-family: Menlo, Consolas, Courier, monospace !important; font-size: 14px; background: transparent !important; }',
+    'code, pre { font-family: Menlo, Consolas, Courier, monospace !important; font-size: 14px; background: transparent !important; }',
+    '[data-prefers-color=dark] .markdown a { color: #844d28 !important; }',
     'pre { color: #333 !important; }',
     'h1 code, h2 code, h3 code, h4 code, h5 code, h6 code { font-size: inherit; }',
     'https://cdn.jsdelivr.net/npm/prism-themes@1.5.0/themes/prism-vs.css',
+    '[data-prefers-color=dark] .__dumi-default-menu-inner ul li a:hover, [data-prefers-color=dark] .__dumi-default-menu-inner ul li > span:hover, [data-prefers-color=dark] .__dumi-default-menu-inner ul li a.active, [data-prefers-color=dark] .__dumi-default-menu-inner ul li > span.active { color: #844d28 !important; }',
+    '[data-prefers-color=dark] code[class*="language-"], [data-prefers-color=dark] pre[class*="language-"] { color: white !important; }',
+    '[data-prefers-color=dark] .markdown *:not(pre) code { color: #844d28 !important; }',
+    '.__dumi-default-menu[data-mode=\'site\'] { padding-top: 0 !important; }',
+    '[data-prefers-color=dark] .__dumi-default-navbar nav > span > a:hover, [data-prefers-color=dark] .__dumi-default-navbar nav > span > a.active, [data-prefers-color=dark] .__dumi-default-layout-footer-meta, [data-prefers-color=dark] .__dumi-default-layout-footer-meta > span:last-child::before { color: #844d28 !important; }',
+    '.__dumi-default-menu[data-mode=\'site\'] .__dumi-default-menu-list { margin-top: 50px; }',
   ],
   favicon: '/public/images/favicon.ico',
   resolve: {
@@ -49,14 +56,14 @@ export default defineConfig({
     previewLangs: [],
   },
   navs: {
-    en: [
+    'en-US': [
       null,
       {
         title: 'GitHub',
         path: 'https://github.com/dolliejs/dollie-core',
       },
     ],
-    zh: [
+    'zh-CN': [
       null,
       {
         title: 'GitHub',
---
+++ docs/README.zh-CN.md
@@ -0,0 +1,52 @@
+---
+title: Dollie.js
+hero:
+  title: Dollie.js
+  desc: Dollie 是初始化工程代码的工具链，它可以帮助你构建成熟的工程化方案。
+  actions:
+    - text: 快速上手
+      link: /zh-CN/guide
+    - text: CLI 入门
+features:
+  - title: 云端化
+    desc: 通过云端托管模板，以及模板与生成器分离的设计，极大提升模板升级迭代的灵活性
+  - title: 易上手
+    desc: 模板配置项简单，易于理解，较短的学习时间即可上手；编写极少代码甚至无需编写代码即可快速制作脚手架
+  - title: 增量覆盖
+    desc: 支持通过扩展模板增量覆盖，从而使定制与选择模板技术栈更加方便快捷
+footer:
+  遵循 MIT 开源协议
+  <br />版权所有 © 2021 至今，Dollie.js 及其贡献者
+  <br />由 [dumi](https://d.umijs.org) 提供支持
+---
+
+## 谁在使用
+
+<table>
+  <tr>
+    <td width="160" align="center">
+      <a target="_blank" href="https://team.ncuos.com">
+        <img width="100" src="https://avatars.githubusercontent.com/u/18623963?s=200&v=4" />
+        <br />
+        <strong>南昌大学家园工作室</strong>
+      </a>
+    </td>
+</table>
+
+## 参与项目开发
+
+我们提供参与 Dollie.js 开发工作的机会，并欢迎任何有意愿参与到这项工作当中的人。但在此之前，请先阅读 Dollie.js 的[行为准则](https://github.com/dolliejs/dollie/blob/master/CODE_OF_CONDUCT.md)。
+
+## 赞助
+
+我们接受赞助，并承诺将 100% 的赞助费用都用于维护 Dollie.js，包括但不限于购买、维护 Dollie.js 文档的域名、服务器，以及支付部分核心贡献者的津贴。
+
+在发起赞助前，请将带有你的姓名、国籍、所在国家或地区的证件号码（可以提供身份证明编号、所在国家或地区的驾驶证编号、VISA 卡号、护照编号等）、Dollie.js 帮助你解决了什么问题（可选）、感谢语（可选）等信息发送给 [dolliejs@lenconda.top](mailto:dolliejs@lenconda.top)，在审核通过后我们将会回复一封带有收款方式的电子邮件，你可以通过这封邮件完成赞助。
+
+非常感谢你对 Dollie.js 项目及其开发者的支持！
+
+<img
+  width="200"
+  style="display: block; margin: 50px auto;"
+  src="//cdn.jsdelivr.net/gh/dolliejs/dollie@master/artworks/banner_horizontal.svg"
+/>
