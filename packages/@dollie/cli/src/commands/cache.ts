1a1bde84eba08ac86417f125b3433b05bd706849
lenconda <i@lenconda.top>
2021-07-17 15:23:40 +0800

    feat: 🎸 finish support for cli cache utils

--- packages/@dollie/cli/src/commands/cache.ts
+++ packages/@dollie/cli/src/commands/cache.ts
@@ -1,5 +1,11 @@
 import commander from 'commander';
 import _ from 'lodash';
+import Table from 'cli-table3';
+import fs from 'fs-extra';
+import {
+  CACHE_DIR,
+} from '../constants';
+import path from 'path';
 
 export default () => {
   const command = new commander.Command('cache');
@@ -10,7 +16,23 @@ export default () => {
     .command('list')
     .description('list all cache items')
     .action(() => {
-      // TODO:
+      let cachedTemplateLabels: string[] = [];
+
+      try {
+        cachedTemplateLabels = fs.readdirSync(CACHE_DIR);
+      } catch {}
+
+      if (cachedTemplateLabels.length > 0) {
+        const table = new Table({
+          head: ['Index', 'URL'],
+        });
+
+        for (const [index, cachedTemplateLabel] of cachedTemplateLabels.entries()) {
+          table.push([index, Buffer.from(cachedTemplateLabel, 'base64').toString()]);
+        }
+
+        console.log(table.toString());
+      }
     });
 
   command
@@ -18,14 +40,31 @@ export default () => {
     .description('delete a cache item by index')
     .arguments('[index]')
     .action((index: string) => {
-      // TODO:
+      let cachedTemplateLabels: string[] = [];
+
+      try {
+        cachedTemplateLabels = fs.readdirSync(CACHE_DIR);
+      } catch {}
+
+      const templateLabel = cachedTemplateLabels[index];
+
+      if (!templateLabel || !_.isString(templateLabel)) {
+        return;
+      }
+
+      fs.removeSync(path.resolve(CACHE_DIR, templateLabel));
     });
 
   command
     .command('clear')
     .description('clear all cached templates')
     .action(() => {
-      // TODO:
+      if (!fs.existsSync(CACHE_DIR)) {
+        return;
+      }
+
+      fs.removeSync(CACHE_DIR);
+      fs.mkdirpSync(CACHE_DIR);
     });
 
   return command;
