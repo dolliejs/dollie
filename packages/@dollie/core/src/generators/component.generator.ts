cf0c280374af489e30e1bbf4551aed2069a83427
lenconda <i@lenconda.top>
2021-08-19 01:07:05 +0800

    feat: 🎸 add ComponentGenerator

---
+++ packages/@dollie/core/src/generators/component.generator.ts
@@ -0,0 +1,30 @@
+import {
+  GeneratorConfig,
+} from '../interfaces';
+import Generator from './generator.abstract';
+
+class ComponentGenerator extends Generator implements Generator {
+  public constructor(
+    projectName: string,
+    templateId: string,
+    config: GeneratorConfig = {},
+  ) {
+    super(projectName, templateId, config);
+  }
+
+  // TODO: implementation
+  public checkInputs() {}
+  public initialize() {}
+  public checkContext() {}
+  public queryAllTemplateProps() {}
+  public copyTemplateFileToCacheTable() {}
+  public deleteFiles() {}
+  public mergeTemplateFiles() {}
+  public resolveConflicts() {}
+  public runCleanups() {}
+  public getResult() {
+    return null;
+  }
+}
+
+export default ComponentGenerator;
